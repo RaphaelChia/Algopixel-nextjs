e9fff889c57242c01f470edf9a193504b1358bdb
Xin Ying <105095442+channxy@users.noreply.github.com>
2023-11-18 22:04:34 +0800

    updates

--- src/app/courses/holidayclasses/page.tsx
+++ src/app/courses/holidayclasses/page.tsx
@@ -1,10 +1,11 @@
 import PagesBanner from "@/components/Banners/pagesbanner";
+import { HolidayClasses } from "@/components/Courses/holidayclasses";
 
 export default function Page(){
     return (
         <div>
         <PagesBanner link="/courses/holidayclasses" page="Holiday Classes" description=""/>
-
+        <HolidayClasses/>
       </div>
     )
 }
---
+++ src/app/courses/weeklyclasses/page.tsx
@@ -0,0 +1,24 @@
+import PagesBanner from "@/components/Banners/pagesbanner";
+
+export default function Page(){
+    return (
+        <div>
+            <div className="bg-lightblue">
+            <div className="mx-auto max-w-2xl px-4 py-28 sm:px-6 lg:max-w-7xl lg:px-8 text-center">
+            <span className="text-purple font-bold">{description}</span>
+            <h1 className="text-2xl sm:text-6xl font-bold text-gray-700 mt-[5px] mb-[38px]">
+                {page}
+            </h1>
+            <nav>
+                <Link href="/" className="text-darkblue opacity-70 hover:text-purple">
+                {"Home "}
+                </Link>
+                <span className="text-darkblue opacity-85">/ </span> 
+                <Link href={link} className="text-darkblue opacity-70">{page}
+                </Link>
+            </nav>
+            </div>
+        </div>
+      </div>
+    )
+}
