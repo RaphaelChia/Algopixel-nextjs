e516c651507fa17ed80ae2496c178759b340bfc3
RaphaelChia <clone_zone@hotmail.com>
2023-11-18 19:43:13 +0800

    css change for coruse page

--- src/components/Programmes/index.tsx
+++ src/components/Programmes/index.tsx
@@ -38,7 +38,7 @@ const Aboutdata: datatype[] = [
 export default function Programmes() {
   return (
     <div className="">
-      <div className="mx-auto max-w-2xl py-20 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="mx-auto  py-20 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <h3 className="text-3xl sm:text-4xl font-bold text-darkblue text-center my-6">
           Our Programmes
         </h3>
