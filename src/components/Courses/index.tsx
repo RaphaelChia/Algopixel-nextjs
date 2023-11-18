05c57374b48210b23299c5d582eafd095b89fb21
RaphaelChia <clone_zone@hotmail.com>
2023-11-19 01:41:04 +0800

    fixed key problem

--- src/components/Courses/index.tsx
+++ src/components/Courses/index.tsx
@@ -84,7 +84,7 @@ export default function Courses() {
 
                 <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-[40px] mt-12 items-stretch'>
                     {Aboutdata.map((item, i) => (
-                        <CourseCard elementKey={i} item={item}/>
+                        <CourseCard key={i} elementKey={i} item={item}/>
                     ))}
                 </div>
             </div>
